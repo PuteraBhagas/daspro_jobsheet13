0a211d49affe38a7fc56aa4f130e14ec393323bf
PuteraBhagas <p.bhagaswara@gmail.com>
2023-12-13 06:56:56 +0700

    Perocbaan

---
+++ Kubus25.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Kubus25 {
+
+    static int Volume (int sisi) {
+        int volume =sisi * sisi * sisi;
+        return volume;
+    }
+    static int LuarPermukaan (int sisi){
+        int luarpermukaan = 6 * (sisi * sisi);
+        return luarpermukaan;
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Masukkan panjang sisi");
+        int p = input.nextInt();
+
+        int V = Volume(p);
+        System.out.println("Volume kubus tersebut adalah : " + V);
+
+        int LP = LuarPermukaan(p);
+        System.out.println("Luas Permukaan kubus tersebut adalah "+LP);
+    }
+    
+}
--- UcapanTerimaKasih_25.java
+++ UcapanTerimaKasih_25.java
@@ -6,18 +6,26 @@ public class UcapanTerimaKasih_25 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan");
         String namaOrang = sc.nextLine();
-        sc.close();
         return namaOrang;
     }
 
     public static void UcapanTerimaKasih() {
         String nama = PenerimaUcapan();
+        String ucapantambah =  UcapanTambahan();
         System.out.println("Thankyou "+nama+" for being the best teacher in the world.\n"+
         "You inspired in me a love for learning and made me feel like i could ask you anyting.");
+        System.out.println("Ucapan tambahan untuk " + nama + " adalah " +ucapantambah);
     }
 
     public static void main(String[] args) {
         UcapanTerimaKasih();
     }
     
+    public static String UcapanTambahan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan ucapan tambahan yang ingin di  berikan");
+        String ucaptambahan = sc.nextLine();
+        sc.close();
+        return ucaptambahan;
+    }
 }
